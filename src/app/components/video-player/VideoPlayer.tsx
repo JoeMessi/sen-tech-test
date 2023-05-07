c3ac34db42dd7f4621dbe319ae108ace9415f205
Joe Messina <giuseppe91messina@gmail.com>
2023-05-07 23:25:43 +0100

    config

--- src/app/components/video-player/VideoPlayer.tsx
+++ src/app/components/video-player/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import ReactPlayer from "react-player/lazy";
+import ReactPlayer from "react-player/youtube";
 import * as S from "./VideoPlayer.styles";
 import { Link } from "react-router-dom";
 import { RoutePaths } from "src/app/routes/route-paths";
@@ -33,36 +33,30 @@ export const VideoPlayer = ({
           controls={true}
           playsinline={true}
           config={{
-            file: {
-              forceVideo: true,
-              forceAudio: true,
-            },
-            youtube: {
-              playerVars: {
-                autoplay: 1,
-                controls: 1,
-                modestbranding: 1,
-                rel: 0,
-                showinfo: 0,
-                playsinline: 1,
-                origin: window.location.origin,
-                // Enable adaptive bitrate
-                hls: {
-                  adaptive: true,
-                },
-                // Enable hardware acceleration
-                playerVars: {
-                  hardwareAcceleration: 1,
-                },
-                // Choose the best video format for the web
-                // 22: 720p, 43: 360p, 18: 360p, 36: 240p, 17: 144p
-                fmt: "22+251/43+251/18+251/36+251/17",
-                // Use video compression to improve load time
-                // 1: Fast Start, 3: Use GPU, 15: Use compression
-                iv_load_policy: 1,
-                ecver: 3,
-                enablejsapi: 1,
+            playerVars: {
+              autoplay: 1,
+              controls: 1,
+              modestbranding: 1,
+              rel: 0,
+              showinfo: 0,
+              playsinline: 1,
+              origin: window.location.origin,
+              // Enable adaptive bitrate
+              hls: {
+                adaptive: true,
               },
+              // Enable hardware acceleration
+              playerVars: {
+                hardwareAcceleration: 1,
+              },
+              // Choose the best video format for the web
+              // 22: 720p, 43: 360p, 18: 360p, 36: 240p, 17: 144p
+              fmt: "22+251/43+251/18+251/36+251/17",
+              // Use video compression to improve load time
+              // 1: Fast Start, 3: Use GPU, 15: Use compression
+              iv_load_policy: 1,
+              ecver: 3,
+              enablejsapi: 1,
             },
           }}
         />
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js",
+    filename: "[name].[contenthash].js",
     publicPath: "/",
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
+        test: /\.(js|ts)x?$/,
         exclude: /node_modules/,
         use: "babel-loader",
       },
@@ -28,6 +29,21 @@ module.exports = {
     extensions: [".jsx", ".tsx", ".ts", ".js", "..."],
   },
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+    splitChunks: {
+      chunks: "all",
+    },
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
