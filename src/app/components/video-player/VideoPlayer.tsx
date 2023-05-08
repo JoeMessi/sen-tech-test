ab79d704a2e1e8abcdfe7a9063d85fa67db612e4
Joe Messina <giuseppe91messina@gmail.com>
2023-05-08 13:46:22 +0100

    more config

--- src/app/components/video-player/VideoPlayer.tsx
+++ src/app/components/video-player/VideoPlayer.tsx
@@ -47,10 +47,9 @@ export const VideoPlayer = ({
               playerVars: {
                 hardwareAcceleration: 1,
               },
-              // Choose the best video format for the web
-              // 22: 720p, 43: 360p, 18: 360p, 36: 240p, 17: 144p
+              // format for the web
               fmt: "22+251/43+251/18+251/36+251/17",
-              // Use video compression to improve load time
+              // video compression to improve load time
               // 1: Fast Start, 3: Use GPU, 15: Use compression
               iv_load_policy: 1,
               ecver: 3,
