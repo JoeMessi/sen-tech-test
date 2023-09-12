0da34a1d5a07df67129a82eb0a410144374c65d7
Joe Messina <giuseppe91messina@gmail.com>
2023-09-12 19:39:54 +0100

    drawing board

--- src/app/routes/route-paths.ts
+++
@@ -1,4 +0,0 @@
-export const RoutePaths = {
-  HOME: "/",
-  PLAYER: (videoId: string) => `video/${videoId}`,
-};
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { App } from "./app/App";
 
 const rootNode = document.getElementById("app");
 if (!rootNode) throw new Error("root node not found!");
@@ -10,6 +8,6 @@ const root = createRoot(rootNode);
 
 root.render(
   <StrictMode>
-    <App />
+    <>Build Me!</>
   </StrictMode>
 );
